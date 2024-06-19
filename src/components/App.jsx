ffbb61d1314aa30bf14446c3052c98287494bd4a
joepherdegz <142654533+joepherdegz@users.noreply.github.com>
2024-06-18 20:09:52 -0700

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -23,16 +23,7 @@ export class App extends Component {
     if (savedContacts !== null) {
       this.setState({ contacts: JSON.parse(savedContacts) });
     }
-    // else {
-    //    this.setState({
-    //      contacts: [
-    //       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    //        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    //         { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    //       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    //    ],
-    //    });
-    //  }
+     
     console.log('componentDidMount()');
   }
 
