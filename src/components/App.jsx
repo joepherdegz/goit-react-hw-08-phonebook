170e3aa869f1b6f4781d4cb4ebb65ae9ae832390
joepherdegz <142654533+joepherdegz@users.noreply.github.com>
2024-06-19 09:17:32 -0700

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,9 +22,16 @@ export class App extends Component {
 
     if (savedContacts !== null) {
       this.setState({ contacts: JSON.parse(savedContacts) });
-    }
-     
-    console.log('componentDidMount()');
+    } else {
+    this.setState({
+        contacts: [
+          { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+          { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+          { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+          { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        ],
+      });
+     }
   }
 
 // componentDidUpdate() after a component is updated
@@ -35,8 +42,6 @@ componentDidUpdate(_prevProps, prevState) {
     if (contacts !== prevState.contacts) {
       localStorage.setItem('contacts', JSON.stringify(contacts));
     }
-
-    console.log('componentDidUpdate()');
   }
   
   addContact = newContact => {
